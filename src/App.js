45f77251649434d45219d17bf36d6ac07417f3f5
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2021-08-29 14:58:54 +0100

    Fetch data from TMDB API
    Render first page of movies object

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 //Components
 import Header from './components/Header';
-
+import Home from './components/Home';
 
 //Styles
-import { GlobalStyle} from './GlobalStyle'
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      Start here.
-      <GlobalStyle/>
+    <div className='App'>
+      <Header />
+      <Home />
+
+      <GlobalStyle />
     </div>
   );
 }
