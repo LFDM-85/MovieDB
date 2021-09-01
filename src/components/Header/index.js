994163de1372f4e68d1decbabacd70e5f0331459
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2021-09-01 21:24:14 +0100

    Routing Setup part2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import MovieDBLogo from '../../images/react-movie-logo.svg'
-import TMDBLogo from '../../images/tmdb_logo.svg'
+import MovieDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
 const Header = () => (
-    <Wrapper>
-        <Content>
-            <LogoImg src={MovieDBLogo} alt='MovieDB-logo'/>
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo'/>
-        </Content>
-    </Wrapper>
+  <Wrapper>
+    <Content>
+      <Link to='/'>
+        <LogoImg src={MovieDBLogo} alt='MovieDB-logo' />
+      </Link>
+      <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+    </Content>
+  </Wrapper>
 );
 
 export default Header;
