f3db036e04284443cd604c2699ada9e2c605a720
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2021-09-02 22:34:25 +0100

    MovieInfo component and styles

---
+++ src/components/MovieInfo/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// Components
+import Thumb from '../Thumb';
+
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
+
+// Image
+import NoImage from '../../images/image-not-found-1-scaled-1150x647.png';
+
+// Styles
+import { Wrapper, Content, Text } from './MovieInfo.styles';
+
+const MovieInfo = ({ movie }) => (
+  <Wrapper backdrop={movie.backdrop_path}>
+    <Content>
+      <Thumb
+        image={
+          movie.poster_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+            : NoImage
+        }
+        clickable={false}
+      />
+      <Text>
+        <h1>{movie.title}</h1>
+        <h3>Plot :</h3>
+        <p>{movie.overview}</p>
+
+        <div className='rating-directors'>
+          <div>
+            <h3>Rating:</h3>
+            <div className='score'>{movie.vote_average}</div>
+          </div>
+          <div className='director'>
+            <h3>Director:{movie.directors.length > 1 ? 's' : ''}</h3>
+            {movie.directors.map((director) => (
+              <p key={director.credit_id}>{director.name}</p>
+            ))}
+          </div>
+        </div>
+      </Text>
+    </Content>
+  </Wrapper>
+);
+
+export default MovieInfo;
