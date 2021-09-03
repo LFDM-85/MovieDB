35a76d4d1b5e199aa6c343168fcda98ea5b455ac
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2021-09-03 19:53:01 +0100

    Actor component and styles
    A few changes in the layout and colors

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -7,8 +7,10 @@ export const Wrapper = styled.div`
   background: var(--darkGrey);
 
   h1 {
+    display: flex;
+    justify-content: center;
     margin: 0;
-    padding: 20px 0;
+    padding: 25px 0;
     color: var(--white); //var(--darkGrey);
     @media screen and (max-width: 768px) {
       font-size: var(--fontBig);
@@ -18,6 +20,10 @@ export const Wrapper = styled.div`
 
 export const Content = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(4, minmax(200px, 1fr));
   grid-gap: 2rem;
+
+  @media screen and (max-width: 950px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
 `;
